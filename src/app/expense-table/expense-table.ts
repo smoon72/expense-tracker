501b7fbdb6afaa870bb2e7cdb9bf5c59a9b29833
smoon72 <sjm9219@gmail.com>
2025-10-18 01:08:07 -0400

    Added a table and a button

--- src/app/expense-table/expense-table.ts
+++ src/app/expense-table/expense-table.ts
@@ -2,17 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { Transaction } from '../../models/Transaction';
 import { CommonModule } from '@angular/common';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { MenuItem, PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-expense-table',
   imports: [
+    ButtonModule,
+    CommonModule,
     TableModule,
-    CommonModule
+    ToolbarModule
   ],
   templateUrl: './expense-table.html',
   styleUrl: './expense-table.scss'
 })
 export class ExpenseTable implements OnInit{
+  items: MenuItem[] = [
+    {
+      label: 'Add',
+      icon: PrimeIcons.PLUS_CIRCLE,
+      command: () => this._add()
+    }
+  ];
 
   transactions: Transaction[] = [
     {
@@ -40,5 +52,10 @@ export class ExpenseTable implements OnInit{
   // }
 
   ngOnInit(): void {
+
+  }
+
+  private _add() {
+    
   }
 }
